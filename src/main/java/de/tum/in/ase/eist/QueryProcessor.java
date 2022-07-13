5e728e5b201717b8fb6793a035129183256a7689
julehendricks <jule.hendricks>
2022-07-13 13:22:15 +0200

    int

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,24 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Jule";
-        } else { // TODO extend the programm here
+        }
+        else if (query.contains("plus")) {
+            String[] wordList = query.split(" ");
+            int intOfWord;
+            int sum = 0;
+            for (String word: wordList) {
+                try {
+                    intOfWord = Integer.parseInt(word);
+                    sum += intOfWord;
+                }
+                catch (Exception e) {
+
+                }
+            }
+            return String.valueOf(sum);
+        }
+
+        else { // TODO extend the programm here
             return "";
         }
         //hello world
